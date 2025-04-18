7ce5fffd19a120f429b8b0f6ba1358949e826c1d
Nicolas Francois Georges Jouslin <nicjousl@bess-f2r1s9.clusters.42paris.fr>
2025-04-18 14:22:40 +0200

    Bientot en ligne

--- frontend/script/installnginx.sh
+++ frontend/script/installnginx.sh
@@ -1,8 +1,9 @@
 apt update
-apt add openssl
+apt install -y openssl
 mkdir -p /certs
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
-       -subj "/C=FR/ST=Paris/L=Paris/O=42/CN=localhost" \
+       -subj "/C=FR/ST=Paris/L=Paris/O=42/CN=62.210.34.175" \
        -out  /certs/cert.crt\
-       -keyout  /certs/cert.key\
- echo "🚀 Lancement du Front..."
+       -keyout  /certs/cert.key
+echo "🚀 Lancement du Front..."
+
--- project-sheets/README.md
+++ project-sheets/README.md
@@ -3,3 +3,4 @@
 Pour lancer le site:
     -docker compose up --build
     -https://localhost:8443
+    -https://62.210.34.175:8443/
