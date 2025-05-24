bcb0c96f0186afbef258776f536ed47645741d7a
Yann Manchon <ymanchon@f2r10s13.clusters.42paris.fr>
2025-05-24 18:03:17 +0200

    HEAD merge fixes

--- frontend/src/profile.ts
+++ frontend/src/profile.ts
@@ -28,10 +28,6 @@ async function displayAvatar(username: string): Promise<void> {
 			img.src = imageUrl;
 		}
 	} catch (err) {
-<<<<<<< HEAD
-=======
-		// alert(";");
->>>>>>> origin/main
 		console.error("Error fetching avatar:", err);
 	}
 }
